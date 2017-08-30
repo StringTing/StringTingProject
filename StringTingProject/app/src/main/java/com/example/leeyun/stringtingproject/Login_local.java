2216826a7aa080ad6356c1e23e1a6c5384b86399
leeyun1533 <leeyun1533@naver.com>
2017-08-30 17:15:30 +0900

    project name change and compile check

---
+++ StringTingProject/app/src/main/java/com/example/leeyun/stringtingproject/Login_local.java
@@ -0,0 +1,19 @@
+package com.example.leeyun.stringtingproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.leeyun.stringtingproject.R;
+
+
+/**
+ * Created by leeyun on 2017. 8. 30..
+ */
+
+public class Login_local extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_local);
+    }
+
+}
---
+++ StringTingProject/app/src/main/java/com/example/leeyun/stringtingproject/Membership_form.java
@@ -0,0 +1,20 @@
+package com.example.leeyun.stringtingproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.leeyun.stringtingproject.R;
+
+
+/**
+ * Created by leeyun on 2017. 8. 30..
+ */
+
+public class Membership_form extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.membership);
+    }
+
+}
