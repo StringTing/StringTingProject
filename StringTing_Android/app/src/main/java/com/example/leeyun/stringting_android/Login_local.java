250fefd4d31e31d0dfefabbc530d910ecb51a164
leeyun1533 <leeyun1533@naver.com>
2017-09-09 00:16:12 +0900

    error_edit

---
+++ StringTing_Android/app/src/main/java/com/example/leeyun/stringting_android/Login_local.java
@@ -0,0 +1,24 @@
+package com.example.leeyun.stringting_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by leeyun on 2017. 9. 8..
+ */
+
+/**
+ * Created by leeyun on 2017. 8. 24..
+ */
+public class Login_local extends Activity {
+
+
+
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.login_local);
+        }
+
+
+
+}
---
+++ StringTing_Android/app/src/main/java/com/example/leeyun/stringting_android/Membership_form.java
@@ -0,0 +1,17 @@
+package com.example.leeyun.stringting_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by leeyun on 2017. 9. 8..
+ */
+
+public class Membership_form extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.membership);
+    }
+
+}
