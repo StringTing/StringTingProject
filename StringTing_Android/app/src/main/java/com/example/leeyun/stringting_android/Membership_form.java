a964b49d7daed528de90c317b0c6b163604358a9
leeyun1533 <leeyun1533@naver.com>
2017-09-16 03:46:43 +0900

    9월16일 회원기본정보입력 segmented_controler 완료

--- StringTing_Android/app/src/main/java/com/example/leeyun/stringting_android/Membership_form.java
+++ StringTing_Android/app/src/main/java/com/example/leeyun/stringting_android/Membership_form.java
@@ -1,7 +1,9 @@
 package com.example.leeyun.stringting_android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by leeyun on 2017. 9. 8..
@@ -12,6 +14,12 @@ public class Membership_form extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.membership);
+    }
+
+
+    public void onClick_Basicinfo_Edit(View v){
+        Intent intent = new Intent(getApplicationContext(),Basicinfo_Edit.class);
+        startActivity(intent);
     }
 
 }
