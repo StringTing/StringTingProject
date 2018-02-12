9f25d75b68f69f85ba3c800afb28ff0a1b5c8461
이정윤 <leeyun1533@naver.com>
2018-02-12 19:14:09 +0900

    이미지업로드 여러개

--- StringTing_Android/app/src/main/java/com/string/leeyun/stringting_android/Mediate.java
+++ StringTing_Android/app/src/main/java/com/string/leeyun/stringting_android/Mediate.java
@@ -12,6 +12,7 @@ import com.string.leeyun.stringting_android.API.Rest_ApiService;
 import com.string.leeyun.stringting_android.API.get_matched_account;
 import com.string.leeyun.stringting_android.API.register_image;
 import com.string.leeyun.stringting_android.API.userinfo;
+import com.string.leeyun.stringting_android.model.api_call;
 import com.tsengvn.typekit.TypekitContextWrapper;
 
 import java.io.File;
@@ -60,6 +61,9 @@ public class Mediate extends AppCompatActivity {
         ImageUploading imageUploading =new ImageUploading();
 //        imageUploading.image_uplodaing_method(Imageupload_countList,token_localdb,account_id_localdb,sex);
         MultipartBody.Part[] images1 = new MultipartBody.Part[0];
+        MultipartBody.Part[] images2 = new MultipartBody.Part[0];
+        MultipartBody.Part[] images3 = new MultipartBody.Part[0];
+
         ArrayList<String> Imageresized_small=new ArrayList<>();
         ArrayList<String>Imageresized_middle=new ArrayList<>();
         ArrayList<String>Imageresized_large=new ArrayList<>();
@@ -107,27 +111,71 @@ public class Mediate extends AppCompatActivity {
 
            images1 = new MultipartBody.Part[Imageprofile1.size()];
 
-            for (int index = 0; index < Imageprofile1.size(); index++) {
-                Log.e("Imageprofile1", Imageprofile1.get(index));
-                    File file = new File(Imageprofile1.get(index));
-                    if (index<3) {
-                        RequestBody surveyBody = RequestBody.create(MediaType.parse("image/*"), file);
-                        images.put("image" + keyvalue.get(index) + "\"; filename=\"" + file.getName(), surveyBody);
+           if (Imageprofile1!=null) {
+               for (int index = 0; index < Imageprofile1.size(); index++) {
+                   Log.e("Imageprofile1", Imageprofile1.get(index));
+                   File file = new File(Imageprofile1.get(index));
+                   if (index < 3) {
+                       RequestBody surveyBody = RequestBody.create(MediaType.parse("image/*"), file);
 
-                        images1[index] = MultipartBody.Part.createFormData("image" + keyvalue.get(index), file.getName(), surveyBody);
-                        Log.e("멀티파트폼 index<3", String.valueOf(images1[index]));
-                    }
-                    if (index==3){
-                        RequestBody surveyBody = RequestBody.create(MediaType.parse("image/*"),"");
-                        images1[index] = MultipartBody.Part.createFormData("image" + keyvalue.get(index), file.getName(), surveyBody);
-                        images.put("image" + keyvalue.get(index), surveyBody);
+                       images1[index] = MultipartBody.Part.createFormData("image" + keyvalue.get(index), file.getName(), surveyBody);
+                       Log.e("멀티파트폼 index<3", String.valueOf(images1[index]));
+                   }
+                   if (index == 3) {
+                       RequestBody surveyBody = RequestBody.create(MediaType.parse("image/*"), "");
+                       images1[index] = MultipartBody.Part.createFormData("image" + keyvalue.get(index), file.getName(), surveyBody);
 
-                        Log.e("멀티파트폼 index==3", String.valueOf(images1[index]));
+                       Log.e("멀티파트폼 index==3", String.valueOf(images1[index]));
 
 //                        images1[index] = MultipartBody.Part.createFormData("image" + keyvalue.get(index), Imageprofile1.get(index), surveyBody);
-                    }
+                   }
 
-            }
+               }
+           }
+           if (Imageprofile2!=null) {
+               for (int index = 0; index < Imageprofile2.size(); index++) {
+                   Log.e("Imageprofile1", Imageprofile2.get(index));
+                   File file = new File(Imageprofile2.get(index));
+                   if (index < 3) {
+                       RequestBody surveyBody = RequestBody.create(MediaType.parse("image/*"), file);
+
+                       images2[index] = MultipartBody.Part.createFormData("image" + keyvalue.get(index), file.getName(), surveyBody);
+                       Log.e("멀티파트폼 index<3", String.valueOf(images2[index]));
+                   }
+                   if (index == 3) {
+                       RequestBody surveyBody = RequestBody.create(MediaType.parse("image/*"), "");
+                       images2[index] = MultipartBody.Part.createFormData("image" + keyvalue.get(index), file.getName(), surveyBody);
+
+                       Log.e("멀티파트폼 index==3", String.valueOf(images2[index]));
+
+//                        images1[index] = MultipartBody.Part.createFormData("image" + keyvalue.get(index), Imageprofile1.get(index), surveyBody);
+                   }
+
+               }
+           }
+           if(Imageprofile3!=null){
+               for (int index = 0; index < Imageprofile2.size(); index++) {
+                   Log.e("Imageprofile1", Imageprofile2.get(index));
+                   File file = new File(Imageprofile2.get(index));
+                   if (index < 3) {
+                       RequestBody surveyBody = RequestBody.create(MediaType.parse("image/*"), file);
+                       images.put("image" + keyvalue.get(index) + "\"; filename=\"" + file.getName(), surveyBody);
+
+                       images3[index] = MultipartBody.Part.createFormData("image" + keyvalue.get(index), file.getName(), surveyBody);
+                       Log.e("멀티파트폼 index<3", String.valueOf(images3[index]));
+                   }
+                   if (index == 3) {
+                       RequestBody surveyBody = RequestBody.create(MediaType.parse("image/*"), "");
+                       images3[index] = MultipartBody.Part.createFormData("image" + keyvalue.get(index), file.getName(), surveyBody);
+
+                       Log.e("멀티파트폼 index==3", String.valueOf(images3[index]));
+
+//                        images1[index] = MultipartBody.Part.createFormData("image" + keyvalue.get(index), Imageprofile1.get(index), surveyBody);
+                   }
+
+               }
+           }
+
 
         }catch (Exception e){
             e.printStackTrace();
@@ -166,39 +214,57 @@ public class Mediate extends AppCompatActivity {
                 .build();
 
 
-        apiService= retrofit.create(Rest_ApiService.class);
-        Call<register_image> call = apiService.post_register_image(images1);
-        call.enqueue(new Callback<register_image>() {
-            @Override
-            public void onResponse(Call<register_image> call, Response<register_image> response) {
-                register_image imageresponse=response.body();
-                Log.e("onregistImage",imageresponse.getResult());
+        api_call imageUploadingApi=new api_call();
+        if (images1[0]!=null){
+            imageUploadingApi.image_uploading(images1,token_localdb,account_id_localdb,sex);
+
+        }
+        else if(images2[0]!=null){
+            imageUploadingApi.image_uploading(images2,token_localdb,account_id_localdb,sex);
+
+        }
+        else if(images3[0]!=null){
+            imageUploadingApi.image_uploading(images3,token_localdb,account_id_localdb,sex);
+
+        }
+        else {
+            Log.e("images가 null값이거나 3번째 이미지를 초과했습니다","false");
+        }
+
+
+//        apiService= retrofit.create(Rest_ApiService.class);
+//        Call<register_image> call = apiService.post_register_image(images1);
+//        call.enqueue(new Callback<register_image>() {
+//            @Override
+//            public void onResponse(Call<register_image> call, Response<register_image> response) {
+//                register_image imageresponse=response.body();
+//                Log.e("onregistImage",imageresponse.getResult());
+////                Log.e("onregistImage", imageresponse.getMessage());
+//
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<register_image> call, Throwable t) {
+//                Log.e("onregistImage_fail",t.toString());
+//            }
+//        });
+//        Call<register_image> call2 = apiService.post_register_image_map(images);
+//        call2.enqueue(new Callback<register_image>() {
+//            @Override
+//            public void onResponse(Call<register_image> call2, Response<register_image> response) {
+//                register_image imageresponse=response.body();
+//                Log.e("onregistImage_map",imageresponse.getResult());
 //                Log.e("onregistImage", imageresponse.getMessage());
-
-
-            }
-
-            @Override
-            public void onFailure(Call<register_image> call, Throwable t) {
-                Log.e("onregistImage_fail",t.toString());
-            }
-        });
-        Call<register_image> call2 = apiService.post_register_image_map(images);
-        call2.enqueue(new Callback<register_image>() {
-            @Override
-            public void onResponse(Call<register_image> call2, Response<register_image> response) {
-                register_image imageresponse=response.body();
-                Log.e("onregistImage",imageresponse.getResult());
-                Log.e("onregistImage", imageresponse.getMessage());
-
-
-            }
-
-            @Override
-            public void onFailure(Call<register_image> call2, Throwable t) {
-                Log.e("onregistImage_fail",t.toString());
-            }
-        });
+//
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<register_image> call2, Throwable t) {
+//                Log.e("onregistImage_fail",t.toString());
+//            }
+//        });
 
     }
     public void onClick_TabbedBar(View v){
@@ -206,16 +272,15 @@ public class Mediate extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void get_local_db(){
+    public void get_local_db() {
 
-        SharedPreferences pref = getSharedPreferences("Local_DB",MODE_PRIVATE);
-        sex=pref.getString("sex","notfound");
-        Log.e("sex",sex);
-        account_id_localdb = pref.getInt("account_id",0);
+        SharedPreferences pref = getSharedPreferences("Local_DB", MODE_PRIVATE);
+        sex = pref.getString("sex", "notfound");
+        Log.e("sex", sex);
+        account_id_localdb = pref.getInt("account_id", 0);
         Log.e("local_account", String.valueOf(account_id_localdb));
-        token_localdb=pref.getString("token","?");
-        Log.e("loacal_token",String.valueOf(token_localdb));
-
+        token_localdb = pref.getString("token", "?");
+        Log.e("loacal_token", String.valueOf(token_localdb));
 
     }
 }
